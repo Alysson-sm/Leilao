bcfcf180acdcb59f9bd0e14afee9cd8db0ed5198
Alysson <asm7@aluno.ifal.edu.br>
2021-08-10 21:35:44 -0300

    Modificaçao no metodo (avalia) para correção de um erro.

--- Leilao.js
+++ Leilao.js
@@ -4,37 +4,33 @@ module.exports = class Leilao {
     constructor(desc) {
         this.descricao = desc;
         this.lances = [];
-        this.maiorLance = new Lance('Ninguem',0);
-        this.menorLance = new Lance('Ninguem',0);
+        this.maiorLance = new Lance('Ninguem', 0);
+        this.menorLance = new Lance('Ninguem', Number.MAX_VALUE);
     }
     getDescricao() {
         return this.descricao;
     }
     getLances() {
-        return this.lances; 
+        return this.lances;
     }
-    getMaiorLance(){
+    getMaiorLance() {
         return this.maiorLance;
     }
-    getMenorLance(){
+    getMenorLance() {
         return this.menorLance;
     }
     propoe(lance) {
         this.lances.push(lance);
     }
     avalia() {
-        
         this.lances.forEach(
-
             (lance) => {
                 if (lance.getValor() > this.maiorLance.getValor()) {
-                    this.maiorLance = lance;    
-                }else{
-                    this.menorLance= lance;
+                    this.maiorLance = lance;
                 }
-                  
-                
-                
+                if (lance.getValor() < this.menorLance.getValor()) {
+                    this.menorLance = lance;
+                }
             }
         );
     }
